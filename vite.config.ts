9eb146ee69ad27fcd202cde23cb411dbd2d52ef3
stebor <stefanborgstrom80@gmail.com>
2026-01-05 13:21:47 +0100

    Add time on last commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { execSync } from "node:child_process";
 
-const commitHash = execSync("git rev-parse --short HEAD")
-  .toString()
-  .trim();
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+const buildTime = new Date().toISOString();
 
 export default defineConfig({
   plugins: [react()],
   base: "/darkside-betting/",
   define: {
     __COMMIT_HASH__: JSON.stringify(commitHash),
+    __BUILD_TIME__: JSON.stringify(buildTime),
   },
 });
